3c4497be16c29b8951828d36c5bda6141bc895b1
CoolSpy3 <coolspythree@gmail.com>
2021-11-13 15:04:57 -0800

    reenable normal start code

--- src/main/java/com/coolspy3/csmodloader/Main.java
+++ src/main/java/com/coolspy3/csmodloader/Main.java
@@ -23,10 +23,6 @@ public class Main
 
     public static void main(String[] args)
     {
-        MainWindow.create();
-
-        if (true) return;
-
         String accessToken = null;
         String gameDir = null;
         String username = null;
