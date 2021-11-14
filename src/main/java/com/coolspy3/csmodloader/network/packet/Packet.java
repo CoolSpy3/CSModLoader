98af466ba376671ab0f629ef7a465b7acfa82c3b
CoolSpy3 <coolspythree@gmail.com>
2021-11-13 16:36:49 -0800

    remove unneccessary id variable

--- src/main/java/com/coolspy3/csmodloader/network/packet/Packet.java
+++ src/main/java/com/coolspy3/csmodloader/network/packet/Packet.java
@@ -6,13 +6,6 @@ public abstract class Packet
     public static final Class<VarInt> VAR_INT = VarInt.class;
     public static final Class<VarLong> VAR_LONG = VarLong.class;
 
-    public final int id;
-
-    public Packet(int id)
-    {
-        this.id = id;
-    }
-
     abstract Object[] getValues();
 
     static final class VarInt extends WrapperType<Integer>
