34e445a149e95a5eb5687ce710a62595c1835eab
CoolSpy3 <coolspythree@gmail.com>
2023-06-24 16:46:03 -0700

    fix bug in Utils.randomString

--- src/main/java/com/coolspy3/csmodloader/util/Utils.java
+++ src/main/java/com/coolspy3/csmodloader/util/Utils.java
@@ -311,11 +311,10 @@ public final class Utils
     {
         int leftLimit = 48; // numeral '0'
         int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
         Random random = new Random();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(targetStringLength)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(length)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
 
